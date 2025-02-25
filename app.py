58332113f2d8e41b29c5cde012581994378bdf26
Matheus  Girardi <girardimatheus27@gmail.com>
2025-02-25 12:33:23 -0300

    Add project

---
+++ app.py
@@ -0,0 +1,196 @@
+from flask import Flask, request, jsonify, redirect, url_for, render_template, session, send_file
+import bcrypt
+from fpdf import FPDF
+import csv
+import os
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta_aqui'  
+
+usuarios = {
+    "velorin": {
+        "senha_hash": b'$2b$12$ZdDv3hl4F.XPV6xw6073J.PAjVAIuOhwAM/pLqF8fhERAXWlxVB3C', 
+    },
+    "r.souza": {
+        "senha_hash": b'$2b$12$GQDP2JRI8c/.Kx4IaOvl2e2f/j5ISGtOhz7EIL1IPJWAeXjkJa2/W',  
+    }
+}
+
+def login_required(route):
+    @wraps(route)  
+    def wrapper(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return route(*args, **kwargs)
+    return wrapper
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')  
+        senha = request.form.get('senha')
+
+        if username in usuarios:
+            senha_hash = usuarios[username]['senha_hash']
+
+            if bcrypt.checkpw(senha.encode('utf-8'), senha_hash):
+                session['username'] = username  
+                return jsonify({"status": "success", "message": "Login realizado com sucesso!"})
+            else:
+                return jsonify({"status": "error", "message": "Senha incorreta"}), 401
+        else:
+            return jsonify({"status": "error", "message": "Usuário não encontrado"}), 404
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+CSV_FILE = 'exames.csv'
+
+if not os.path.exists(CSV_FILE):
+    with open(CSV_FILE, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Nome", "Valor"])
+
+@app.route('/exames', methods=['GET'])
+@login_required
+def listar_exames():
+    exames = []
+    with open(CSV_FILE, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  
+        for row in reader:
+            exames.append({"nome": row[0], "valor": row[1]})
+    return jsonify(exames)
+
+@app.route('/cadastrar', methods=['POST'])
+@login_required
+def cadastrar_exame():
+    nome = request.json.get('nome')
+    valor = request.json.get('valor')
+    with open(CSV_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([nome, valor])
+    return jsonify({"status": "success"})
+
+@app.route('/editar_exame', methods=['POST'])
+@login_required
+def editar_exame():
+    nome_antigo = request.json.get('nomeAntigo')
+    nome_novo = request.json.get('nomeNovo')
+    valor = request.json.get('valor')
+
+    exames = []
+    with open(CSV_FILE, 'r') as file:
+        reader = csv.reader(file)
+        exames = list(reader)
+
+    with open(CSV_FILE, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in exames:
+            if row[0] == nome_antigo:
+                writer.writerow([nome_novo, valor])
+            else:
+                writer.writerow(row)
+
+    return jsonify({"status": "success"})
+
+@app.route('/excluir_exame', methods=['POST'])
+@login_required
+def excluir_exame():
+    nome = request.json.get('nome')
+
+    exames = []
+    with open(CSV_FILE, 'r') as file:
+        reader = csv.reader(file)
+        exames = list(reader)
+
+    with open(CSV_FILE, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in exames:
+            if row[0] != nome:
+                writer.writerow(row)
+
+    return jsonify({"status": "success"})
+
+@app.route('/gerar_orcamento', methods=['POST'])
+@login_required
+def gerar_orcamento():
+    from datetime import datetime, timedelta
+    cliente = request.json.get('cliente')
+    cpf = request.json.get('cpf')
+    exames_selecionados = request.json.get('exames')
+
+    data_atual = datetime.now()
+    data_formatada = data_atual.strftime("%d/%m/%Y")
+    data_validade = data_atual + timedelta(days=30)
+    data_validade_formatada = data_validade.strftime("%d/%m/%Y")
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    pdf.set_font("Arial", 'B', 16)
+    pdf.set_text_color(149, 6, 6) 
+    pdf.cell(200, 10, txt="LABORATÓRIO HOLOS", ln=True, align='C')
+    pdf.set_font("Arial", size=12)
+    pdf.set_text_color(63, 23, 23)  
+    pdf.cell(200, 10, txt="Saúde Ocupacional", ln=True, align='C')
+    pdf.ln(10)
+
+    pdf.set_font("Arial", 'B', 14)
+    pdf.set_text_color(149, 6, 6) 
+    pdf.cell(200, 10, txt="ORÇAMENTO", ln=True, align='C')
+    pdf.ln(10)
+
+    pdf.set_font("Arial", size=12)
+    pdf.set_text_color(63, 23, 23)  
+    pdf.cell(200, 10, txt=f"COLABORADOR: {cliente}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"CPF: {cpf}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"ORÇAMENTO REALIZADO NO DIA: {data_formatada}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"VÁLIDO ATÉ O DIA: {data_validade_formatada}", ln=True, align='L')
+    pdf.ln(10)
+
+    pdf.set_font("Arial", 'B', 12)
+    pdf.set_text_color(149, 6, 6)  
+    pdf.cell(95, 10, txt="Exames", border=1, align='C')
+    pdf.cell(95, 10, txt="Total da linha", border=1, align='C', ln=True)
+    pdf.set_font("Arial", size=12)
+    pdf.set_text_color(63, 23, 23)  
+
+    total = 0
+    for exame in exames_selecionados:
+        pdf.cell(95, 10, txt=exame['nome'], border=1, align='L')
+        pdf.cell(95, 10, txt=f"R$ {exame['valor']}", border=1, align='C', ln=True)
+        total += float(exame['valor'])
+
+    pdf.cell(95, 10, txt="Total", border=1, align='L')
+    pdf.cell(95, 10, txt=f"R$ {total:.2f}", border=1, align='C', ln=True)
+    pdf.ln(10)
+
+    pdf.set_font("Arial", size=12)
+    pdf.set_text_color(149, 6, 6)  
+    pdf.cell(200, 10, txt="Obrigado pela preferência!", ln=True, align='C')
+    pdf.ln(10)
+    pdf.set_font("Arial", 'I', 10)
+    pdf.set_text_color(63, 23, 23)  
+    pdf.cell(200, 10, txt="Laboratório Holos", ln=True, align='C')
+    pdf.cell(200, 10, txt="Avenida Doutor Galdino do Valle Filho, N 133, Centro, 28625-010, Nova Friburgo - RJ", ln=True, align='C')
+    pdf.cell(200, 10, txt="(22)9 8837-0724 | @holoservicosmedicos", ln=True, align='C')
+
+    pdf_output = f"orcamento_{cliente}.pdf"
+    pdf.output(pdf_output)
+
+    return send_file(pdf_output, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,201 @@
+document.addEventListener('DOMContentLoaded', () => {
+    carregarExames(); 
+    document.getElementById('form-cadastrar').addEventListener('submit', cadastrarExame);
+    document.getElementById('form-orcamento').addEventListener('submit', gerarOrcamento);
+});
+
+let todosExames = []; 
+let examesSelecionados = new Set(); 
+
+function openTab(tabName) {
+    const tabcontents = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].style.display = 'none';
+    }
+    document.getElementById(tabName).style.display = 'block';
+}
+
+function carregarExames() {
+    fetch('/exames')
+        .then(response => response.json())
+        .then(data => {
+            todosExames = data; 
+            atualizarListaExames(data); 
+            atualizarListaExamesOrcamento(data); 
+        });
+}
+
+function atualizarListaExames(exames) {
+    const listaExames = document.getElementById('lista-exames');
+    listaExames.innerHTML = '';
+
+    exames.forEach(exame => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+            <h3>${exame.nome}</h3>
+            <p>R$ ${exame.valor}</p>
+            <button onclick="editarExame('${exame.nome}', '${exame.valor}')">Editar</button>
+            <button onclick="excluirExame('${exame.nome}')">Excluir</button>
+        `;
+        listaExames.appendChild(card);
+    });
+}
+
+function filtrarExames() {
+    const termo = document.getElementById('pesquisa-exames').value.toLowerCase().substring(0, 3); 
+    const examesFiltrados = todosExames.filter(exame => exame.nome.toLowerCase().startsWith(termo));
+    atualizarListaExames(examesFiltrados); 
+}
+
+function filtrarExamesOrcamento() {
+    const termo = document.getElementById('pesquisa-orcamento').value.toLowerCase().substring(0, 3); 
+    const examesFiltrados = todosExames.filter(exame => exame.nome.toLowerCase().startsWith(termo));
+
+    atualizarListaExamesOrcamento(examesFiltrados);
+
+    examesSelecionados.forEach(nome => {
+        const checkbox = document.querySelector(`input[name="exames"][value="${nome}"]`);
+        if (checkbox) {
+            checkbox.checked = true;
+        }
+    });
+}
+
+function atualizarListaExamesOrcamento(exames) {
+    const listaExamesOrcamento = document.getElementById('lista-exames-orcamento');
+    listaExamesOrcamento.innerHTML = '';
+
+    exames.forEach(exame => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'exames';
+        checkbox.value = exame.nome;
+        checkbox.dataset.valor = exame.valor;
+
+        if (examesSelecionados.has(exame.nome)) {
+            checkbox.checked = true;
+        }
+
+        checkbox.addEventListener('change', (event) => {
+            if (event.target.checked) {
+                examesSelecionados.add(exame.nome);
+            } else {
+                examesSelecionados.delete(exame.nome);
+            }
+        });
+
+        const label = document.createElement('label');
+        label.textContent = `${exame.nome} - R$ ${exame.valor}`;
+        label.appendChild(checkbox);
+
+        listaExamesOrcamento.appendChild(label);
+        listaExamesOrcamento.appendChild(document.createElement('br'));
+    });
+}
+
+function cadastrarExame(event) {
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const valor = document.getElementById('valor').value;
+
+    fetch('/cadastrar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nome, valor }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        mostrarMensagem('Exame cadastrado com sucesso!');
+        carregarExames(); 
+        limparCamposCadastro(); 
+    });
+}
+
+function limparCamposCadastro() {
+    document.getElementById('nome').value = '';
+    document.getElementById('valor').value = '';
+}
+
+function editarExame(nome, valor) {
+    const novoNome = prompt("Editar nome do exame:", nome);
+    const novoValor = prompt("Editar valor do exame:", valor);
+
+    if (novoNome && novoValor) {
+        fetch('/editar_exame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nomeAntigo: nome, nomeNovo: novoNome, valor: novoValor }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            mostrarMensagem('Exame editado com sucesso!');
+            carregarExames(); 
+        });
+    }
+}
+
+function excluirExame(nome) {
+    if (confirm(`Tem certeza que deseja excluir o exame "${nome}"?`)) {
+        fetch('/excluir_exame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nome }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            mostrarMensagem('Exame excluído com sucesso!');
+            carregarExames(); 
+        });
+    }
+}
+
+function gerarOrcamento(event) {
+    event.preventDefault();
+    const cliente = document.getElementById('cliente').value;
+    const cpf = document.getElementById('cpf').value;
+    const examesSelecionadosArray = Array.from(document.querySelectorAll('input[name="exames"]:checked'))
+        .map(checkbox => ({ nome: checkbox.value, valor: checkbox.dataset.valor }));
+
+    fetch('/gerar_orcamento', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cliente, cpf, exames: examesSelecionadosArray }),
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `orcamento_${cliente}.pdf`;
+        a.click();
+        limparCamposOrcamento(); 
+    });
+}
+
+function limparCamposOrcamento() {
+    document.getElementById('cliente').value = '';
+    document.getElementById('cpf').value = '';
+    const checkboxes = document.querySelectorAll('input[name="exames"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+    examesSelecionados.clear(); 
+}
+
+function mostrarMensagem(mensagem, tipo = 'success') {
+    const mensagemDiv = document.createElement('div');
+    mensagemDiv.className = 'mensagem';
+    mensagemDiv.textContent = mensagem;
+    mensagemDiv.style.backgroundColor = tipo === 'error' ? '#950606' : '#4CAF50'; 
+    document.body.appendChild(mensagemDiv);
+    setTimeout(() => mensagemDiv.remove(), 3000);
+}
