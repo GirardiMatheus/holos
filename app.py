0ca396049b7479fc57156d38e5909002b6e5a143
Matheus  Girardi <girardimatheus27@gmail.com>
2025-02-25 12:52:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def gerar_orcamento():
 if __name__ == '__main__':
     PORT = int(os.environ.get("PORT", 5000))
 
-    app.run(host="0.0.0.0", port=PORT))
+    app.run(host="0.0.0.0", port=PORT)
